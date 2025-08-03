5893a1c0b92bf8a17d645535a515ee50b0f8d3b0
MinhTuan <tuanmt2912@gmail.com>
2025-08-03 11:39:17 +0700

    Demo DAO in Spring boot with model Products

---
+++ src/main/java/org/example/DAO/service/ProductServiceImpl.java
@@ -0,0 +1,46 @@
+package org.example.DAO.service;
+
+import org.example.DAO.model.Product;
+import org.example.DAO.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public Product findByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    @Override
+    public List<Product> findProductsByPriceGreaterThan(Double price) {
+        return productRepository.findProductsByPriceGreaterThan(price);
+    }
+}
