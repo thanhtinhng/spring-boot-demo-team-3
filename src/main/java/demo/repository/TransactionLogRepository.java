b30a924cc22cb2e426579d553a2ec0f188b889f8
tranvananhanhanh <khuctieuho@gmail.com>
2025-08-04 11:46:38 +0700

    update entity

--- src/main/java/demo/repository/TransactionLogRepository.java
+++
@@ -1,7 +0,0 @@
-package demo.repository;
-
-import demo.entity.TransactionLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionLogRepository extends JpaRepository<TransactionLog, Long> {
-}
